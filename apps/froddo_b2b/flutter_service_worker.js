39ca158c73e1fbd82071cb4e70cbb7d556c4c363
Matan <matan.ljubas@3ofthem.eu>
2025-07-15 17:48:47 +0200

    Latest

--- apps/froddo_b2b/flutter_service_worker.js
+++ apps/froddo_b2b/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0ff50bcd55e24b94996a704cf6b408c9",
+const RESOURCES = {"flutter_bootstrap.js": "71c5665b23d22c33b4136b61602b8f35",
 "version.json": "a7eee6010b54435174df90a9706f3c34",
 "index.html": "7e3967bfeafbe9af34225c19085bcacc",
 "/": "7e3967bfeafbe9af34225c19085bcacc",
-"main.dart.js": "0bc5efc76068400c7863c0a05aa9f87c",
+"main.dart.js": "12249dee45086ffe450bbe5957b3a786",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
