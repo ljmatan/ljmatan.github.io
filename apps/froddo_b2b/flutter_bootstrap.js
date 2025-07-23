2b89063e3d89cc1014f77ec3eeb09db79dd35c39
Matan <matan.ljubas@3ofthem.eu>
2025-07-23 22:06:32 +0200

    Latest

--- apps/froddo_b2b/flutter_bootstrap.js
+++ apps/froddo_b2b/flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"b9d245928a533a68d86c19a6028df5b15220488c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"70a3c3cc935e49b44db818348dd2d57f055ab762","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3467926195"
+    serviceWorkerVersion: "3051787157"
   }
 });
