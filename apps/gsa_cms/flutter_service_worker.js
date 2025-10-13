a52e153ebb51de08f4852cb5a540236e46780527
Matan <matan.ljubas@3ofthem.eu>
2025-10-13 23:14:17 +0200

    Latest

--- apps/gsa_cms/flutter_service_worker.js
+++ apps/gsa_cms/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c59af7d6ffb907ac0bd146eeca39a689",
+const RESOURCES = {"flutter_bootstrap.js": "15407d06bf28eac4461a8362a083e0dd",
 "version.json": "893eda7bc35b60c970114a8644649c91",
 "index.html": "261eb316b398711c9aa89059f961f67d",
 "/": "261eb316b398711c9aa89059f961f67d",
-"main.dart.js": "d0306daebcda60199be1bdc29c022032",
+"main.dart.js": "e73addc99daa803dbb42b26c8314834d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
